079d97c30813e424abe4efedf490a05de8c736cd
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-02 20:10:45 -0700

    add randomness

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,50 @@
+import clsx from 'clsx'
+import { useState } from 'react'
+
 export function App() {
+  const cards = Array.from({ length: 100 }, (_, i) => i)
   return (
-    <div className="flex flex-col">
-      <Card />
+    <div
+      className={clsx(
+        'flex flex-col',
+        'gap-4 p-4',
+        'md:gap-8 md:p-8',
+      )}
+    >
+      {cards.map((i) => (
+        <Card key={i} />
+      ))}
     </div>
   )
 }
 
 function Card() {
+  const [state] = useState(() => {
+    const { h, s, l } = (() => {
+      const r = Math.random()
+      if (r > 1 / 2) {
+        return { h: 0, s: 0, l: 10 }
+      } else if (r > 1 / 8) {
+        return { h: 0, s: 0, l: 25 }
+      } else if (r > 1 / 32) {
+        return { h: 0, s: 0, l: 40 }
+      } else {
+        return { h: 60, s: 90, l: 50 }
+      }
+    })()
+    return {
+      bg: `hsl(${h}, ${s}%, ${l}%)`,
+    }
+  })
+
   return (
-    <div className="p-4">
-      <div className="border border-black rounded overflow-hidden">
-        <div className="aspect-square bg-gray-200 border-b border-black" />
-        <div className="p-2">
-          <h2>Card Component</h2>
-          <p>This is a card component.</p>
-        </div>
-      </div>
+    <div className="rounded overflow-hidden">
+      <div
+        className={clsx('aspect-square')}
+        style={{
+          backgroundColor: state.bg,
+        }}
+      />
     </div>
   )
 }
